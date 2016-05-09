859e66691a13ca271bc504caca521f941b43de51
chinatsp <chinatsp@chinatspdemac-mini.local>
2016-05-09 12:43:04 +0800

    Initial Commit

---
+++ WayMisc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WayMisc
+//
+//  Created by chinatsp on 16/5/9.
+//  Copyright © 2016年 HandsonWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WayMisc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WayMisc
+//
+//  Created by chinatsp on 16/5/9.
+//  Copyright © 2016年 HandsonWu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
